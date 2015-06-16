c6b62254501eb02f48e7bba0f73a037789c9144f
Blake Guilloud <fbguillo@gmail.com>
2015-06-16 13:35:21 -0400

    initial commit

---
+++ templates.js
@@ -0,0 +1,9 @@
+var templates = {};
+
+templates.head = [
+  "<p>Welcome</p>"
+  "<div><h1><%= userName %><h1></div>"
+  "<p>Your Chip Total is: <%= chipTotal %> </p>"
+  "<div class = 'theChipTotal'><h1><%= chipTotal %>.<h1></div>"
+
+]
